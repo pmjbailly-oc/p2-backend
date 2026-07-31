99e1c355bb89384169768c670f58522d2b627b6a
pmjba <pmj.bailly@gmail.com>
2026-07-31 13:47:09 +0200

    Add Student DTOs and MapStruct mapper

---
+++ src/main/java/com/openclassrooms/etudiant/dto/StudentRequestDTO.java
@@ -0,0 +1,17 @@
+package com.openclassrooms.etudiant.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class StudentRequestDTO {
+    @NotBlank
+    private String firstName;
+    @NotBlank
+    private String lastName;
+    @NotBlank
+    @Email
+    private String email;
+
+}
---
+++ src/main/java/com/openclassrooms/etudiant/mapper/StudentDtoMapper.java
@@ -0,0 +1,21 @@
+package com.openclassrooms.etudiant.mapper;
+
+import com.openclassrooms.etudiant.dto.StudentRequestDTO;
+import com.openclassrooms.etudiant.dto.StudentResponseDTO;
+import com.openclassrooms.etudiant.entities.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.mapstruct.ReportingPolicy;
+
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.ERROR)
+public interface StudentDtoMapper {
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "created_at", ignore = true)
+    @Mapping(target = "updated_at", ignore = true)
+    Student toEntity(StudentRequestDTO studentRequestDTO);
+
+    @Mapping(source = "created_at", target = "createdAt")
+    @Mapping(source = "updated_at", target = "updatedAt")
+    StudentResponseDTO toResponseDTO(Student student);
+}
